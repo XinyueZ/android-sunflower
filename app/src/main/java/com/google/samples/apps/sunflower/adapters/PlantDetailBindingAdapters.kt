0c1851a09adcea69ca5c49d4a10f10de940d0dc2
XinyueZ <dev.xinyue.zhao@gmail.com>
2019-03-04 23:28:24 +0100

    Prepare #351

--- app/src/main/java/com/google/samples/apps/sunflower/adapters/PlantDetailBindingAdapters.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/adapters/PlantDetailBindingAdapters.kt
@@ -16,19 +16,15 @@
 
 package com.google.samples.apps.sunflower.adapters
 
-import android.text.SpannableStringBuilder
 import android.text.method.LinkMovementMethod
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.text.HtmlCompat
 import androidx.core.text.HtmlCompat.FROM_HTML_MODE_COMPACT
-import androidx.core.text.bold
-import androidx.core.text.italic
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.samples.apps.sunflower.R
 
 @BindingAdapter("imageFromUrl")
 fun bindImageFromUrl(view: ImageView, imageUrl: String?) {
@@ -57,16 +53,4 @@ fun bindRenderHtml(view: TextView, description: String?) {
     } else {
         view.text = ""
     }
-}
-
-@BindingAdapter("wateringText")
-fun bindWateringText(textView: TextView, wateringInterval: Int) {
-    val resources = textView.context.resources
-    val quantityString = resources.getQuantityString(R.plurals.watering_needs_suffix,
-        wateringInterval, wateringInterval)
-
-    textView.text = SpannableStringBuilder()
-        .bold { append(resources.getString(R.string.watering_needs_prefix)) }
-        .append(" ")
-        .italic { append(quantityString) }
 }
